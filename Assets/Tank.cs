58b070a97b6ad3c1923aa3f3b5b993edccfce762
Hunger720 <Hunger720@gmail.com>
2018-06-07 16:52:18 +0800

    First Script
    First C# Script

---
+++ Assets/Tank.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float speed = 1;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position += transform.forward * speed;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.position += transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 270, 0);
+            transform.position += transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 90, 0);
+            transform.position += transform.forward * speed;
+        }
+
+    }
+}
